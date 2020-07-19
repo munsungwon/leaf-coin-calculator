74cbb0a9730ee2333df0f9c8121ad26847d4a621
munsungwon <44133533+munsungwon@users.noreply.github.com>
2020-07-19 16:34:02 +0900

    Update watermaelon_time_hour.py
    This is the python code for calculating the starting time of leaf browning which caused from emission gas of automotive.

--- watermaelon_time_hour.py
+++ watermaelon_time_hour.py
@@ -5,12 +5,12 @@ import math
 def call_result1(label_result1,n1, n2):
     num1 = (n1.get())
     num2 = (n2.get())
-    hour = 10**(1.998-1.536*math.log10(float(num1)/float(num2)))  ## int -> float 
+    hour = 10**(1.998-1.536*math.log10(float(num1)/float(num2))) 
     label_result1.config(text="Result is %0.2f (hr)" % hour)
 
 def call_result2(label_result2,n1):
     num1 = (n1.get())
-    need = float(num1)/(10**((1.998-math.log10(24))/1.536))  ## int -> float 
+    need = float(num1)/(10**((1.998-math.log10(24))/1.536))
     label_result2.config(text="Result is %0.2f (EA)" % need)
 
 root = tk.Tk()
@@ -35,7 +35,7 @@ entryNum2 = tk.Entry(root, textvariable=number2).grid(row=2, column=1)
 call_result1 = partial(call_result1, labelResult1, number1, number2)
 call_result2 = partial(call_result2, labelResult2, number1)
 
-buttonCal1 = tk.Button(root, text="피해발현시간", command=call_result1).grid(row=4, column=0)
-buttonCal2 = tk.Button(root, text="필요한 수박개수", command=call_result2).grid(row=5, column=0)
+buttonCal1 = tk.Button(root, text="starting time of leaf browning", command=call_result1).grid(row=4, column=0)
+buttonCal2 = tk.Button(root, text="number of watermelon", command=call_result2).grid(row=5, column=0)
 
 root.mainloop()
